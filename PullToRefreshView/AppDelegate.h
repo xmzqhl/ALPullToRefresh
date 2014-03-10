676f1f6530510768a66cc818f1a018a8bde34683
liubo <liubo@kongfz.com>
2014-03-10 14:11:31 +0800

    Initial Commit

---
+++ PullToRefreshView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PullToRefreshView
+//
+//  Created by Arien Lau on 14-3-10.
+//  Copyright (c) 2014年 Arien Lau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PullToRefreshViewTests/PullToRefreshViewTests.m
@@ -0,0 +1,34 @@
+//
+//  PullToRefreshViewTests.m
+//  PullToRefreshViewTests
+//
+//  Created by Arien Lau on 14-3-10.
+//  Copyright (c) 2014年 Arien Lau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PullToRefreshViewTests : XCTestCase
+
+@end
+
+@implementation PullToRefreshViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
